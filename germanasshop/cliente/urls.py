7612f0bd2db25a5ad98af9eebfc1530852577b20
Fernando <fernandosmartphone96@gmail.com>
2022-04-02 15:48:37 -0300

    retira urls desnecessárias de cliente

--- germanasshop/cliente/urls.py
+++ germanasshop/cliente/urls.py
@@ -4,18 +4,6 @@ from . import views
 
 app_name = 'cliente'
 urlpatterns = [
-    # ex: /polls/
-    # path('', views.index, name='index'),
-    # ex: /polls/5/
-    #path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    # path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    #path('<int:question_id>/vote/', views.vote, name='vote'),
-    # ex: /polls/
     path("cadastro/", views.cadastro, name='cadastro'),
-
     path('login/', views.login, name='login'),
-    path('logindjango/',  auth_view.LoginView.as_view(template_name= 'cadastro/login.html') , name='logindjango')
-    
 ]
