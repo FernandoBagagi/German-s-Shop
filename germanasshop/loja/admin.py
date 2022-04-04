938a91424221b17222fc4110d2fd2e8b831aed12
Fernando <fernandosmartphone96@gmail.com>
2022-04-04 20:35:40 -0300

    customiza Admin

--- germanasshop/loja/admin.py
+++ germanasshop/loja/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
-
 from .models import Compra, CompraProduto, Reclamacao, Produto,Carrinho, Favorito
 
-admin.site.register(Produto)
-admin.site.register(Reclamacao)
-admin.site.register(Carrinho)
-admin.site.register(Favorito)
-admin.site.register(Compra)
-admin.site.register(CompraProduto)
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'categoria')
+
+@admin.register(Reclamacao)
+class ReclamacaoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_pergunta', 'data_resposta')
+
+@admin.register(Carrinho)
+class CarrinhoAdmin(admin.ModelAdmin):
+    list_display = ('id_produto', 'id_usuario', 'quantidade')
+
+@admin.register(Favorito)
+class FavoritoAdmin(admin.ModelAdmin):
+    list_display = ('id_usuario', 'id_produto', 'data_adicao')
+
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    list_display = ('id_usuario', 'data', 'total')
+
+@admin.register(CompraProduto)
+class CompraProduto(admin.ModelAdmin):
+    list_display = ('id_produto', 'id_compra',)
+
