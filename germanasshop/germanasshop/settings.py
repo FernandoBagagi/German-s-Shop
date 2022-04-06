8e35ef2df56830aed9a41b214b6d0d7e23aa85e4
YanAnderson-bit <yan.anderson135@gmail.com>
2022-04-05 21:29:26 -0300

    FUNCIONAAAAAAAAAAAAAAAAAAAAAA

--- germanasshop/germanasshop/settings.py
+++ germanasshop/germanasshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4xy1lb@x#&7opm%ha0+w!&old+kcq40&t^3-2^mrj4hemt4w5c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blooming-tundra-55642.herokuapp.com']
 
 # Application definition
 
